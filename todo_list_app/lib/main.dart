c7d1d82799fdccf6e1a317bfdf13ad487f02578b
Surajmandal99 <surajxuraj@gmail.com>
2023-11-16 11:31:54 +0530

    initial_commit

---
+++ todo_list_app/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:todo_list_app/pages/home_page.dart';
+
+void main() async {
+  //initilize the hive
+  await Hive.initFlutter();
+
+  //open a box
+  // ignore: unused_local_variable
+  var box = await Hive.openBox("mybox");
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+    );
+  }
+}
